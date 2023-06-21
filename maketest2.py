4cc9d35369879bce140a92da785fbfa6a24b6b26
Lirovsk <joao.lira@engenharia.ufjf.br>
2023-06-21 17:28:07 -0300

    ...

--- maketest2.py
+++ maketest2.py
@@ -13,12 +13,12 @@ BOARD.blink(0.2, 2)
 
 # construcao do codigo
 lora.set_mode(MODE.STDBY)
-payload = "enviando mensagens pela primeira vez"
+payload = ["enviando mensagens pela primeira vez"]
 lora.write_payload(payload)
 BOARD.blink(0.4, 3)
 lora.set_mode(MODE.SLEEP)
 time.sleep(1)
 lora.set_mode(MODE.STDBY)
-msg = "enviando mensagens pela segunda vez"
+msg = ["enviando mensagens pela segunda vez"]
 lora.write_payload(msg)
 BOARD.blink(0.1, 3)
