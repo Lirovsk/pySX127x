4433aadfab9cef51dc1521eee1452221ac0b3706
Lirovsk <joao.lira@engenharia.ufjf.br>
2023-06-21 17:49:31 -0300

    ..

--- maketest2.py
+++ maketest2.py
@@ -13,12 +13,12 @@ BOARD.blink(0.2, 2)
 
 # construcao do codigo
 lora.set_mode(MODE.STDBY)
-payload = ["2023"]
+payload = [2023]
 lora.write_payload(payload)
 BOARD.blink(0.4, 3)
 lora.set_mode(MODE.SLEEP)
 time.sleep(1)
 lora.set_mode(MODE.STDBY)
-msg = ["2023.2"]
+msg = [2023.2]
 lora.write_payload(msg)
 BOARD.blink(0.1, 3)
